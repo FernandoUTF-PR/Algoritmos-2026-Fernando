ff2e5fe147d76e834feaa94502f1443f2baa2417
FernandoUTF-PR <fernandomoresquimedeiros@gmail.com>
2026-09-14 09:56:49 -0300

    ana com 4 n

---
+++ Algoritmos_14.09.2026/Produto.js
@@ -0,0 +1,11 @@
+//exercício 01 da lista 02 do terceiro bimestre
+
+class Produto {
+    constructor(id, nome, precoUnitario, peso, quantidadeEstoque) {
+        this.id = id;
+        this.nome = nome;
+        this.precoUnitario = precoUnitario;
+        this.peso = peso;
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+}
